0896c13da302853834ac67d79dba9a112d44cf39
grigoryan.andrew <grigoryan.andrew@gmail.com>
2023-07-08 22:05:04 +0300

    navbar

---
+++ src/components/navbar/Navbar.styles.tsx
@@ -0,0 +1,23 @@
+export const NavBarStyles = {
+  background: {
+    backgroundColor: 'black',
+    width: 1,
+    opacity: '82%',
+  },
+  button: {
+    my: 2,
+    color: 'white',
+    display: 'block',
+    fontSize: '17px',
+    padding: '0px 20px',
+    fontFamily: 'Bitter',
+    fontWeight: 400,
+  },
+  menuItem: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: 120,
+    fontFamily: 'Bitter',
+  },
+};
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+import { NavBar } from '../../components/navbar/Navbar';
+import { HomeStyles } from './Home.styles';
+
+export const Home = () => {
+  return (
+    <div className='wrapper'>
+      <NavBar />
+    </div>
+  );
+};
