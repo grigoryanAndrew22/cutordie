c3d4b34a840757dbfd0b6af826157e7e9fec682a
AlexeyArsentyev <alexey10.arsentyev@gmail.com>
2024-04-04 11:06:56 +0300

    view-all-courses fixed

--- src/components/bottom-section/BottomSection.styles.tsx
+++ src/components/bottom-section/BottomSection.styles.tsx
@@ -5,8 +5,6 @@ export const bottomSectionStyles = {
     cursor: 'pointer',
   },
   viewAllCourses: {
-    display: 'flex',
-    justifyContent: 'center',
     margin: '35px 0px 40px 0px',
   },
 };
