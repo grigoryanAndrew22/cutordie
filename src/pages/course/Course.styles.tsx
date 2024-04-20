26947a8863605d6b39cd1bd947962c0c3fa33bbc
AlexeyArsentyev <alexey10.arsentyev@gmail.com>
2024-04-20 10:26:36 +0300

    buynow btn

--- src/pages/course/Course.styles.tsx
+++ src/pages/course/Course.styles.tsx
@@ -1,10 +1,12 @@
+import { Scale } from '@mui/icons-material';
+
 export const courseStyles = {
   buyBtn: {
     background: 'transparent',
     border: 'none',
     cursor: 'pointer',
-    height: 110,
-    width: 260,
+    height: 142,
+    width: 363,
     padding: 0,
     outline: 0,
   },
