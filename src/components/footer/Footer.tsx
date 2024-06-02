bfe274c8a413da555a2106d374dec4a9f0ee2de8
grigoryan.andrew <grigoryan.andrew@gmail.com>
2024-06-02 18:22:52 +0300

    log erased

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -45,8 +45,6 @@ export const Footer = (props: any) => {
     props.changeCurr('eur');
   };
 
-  console.log(props.currency);
-
   const footerTranslated =
     props.language === 'en' ? footerLangs.en : footerLangs.ua;
 
